467d94530a096c23e982c246d724e697fc823188
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 22:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const baseURL = 'https://api.github.com';
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return c92d34b99b3130c0cae7c82af1a9ca02e52593a7;
+  // return c92d34b99b3130c0cae7c82af1a9ca02e52593a7;
+  return ''
 }
 
 function forkRepo() {
