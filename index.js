7401dab9640820634a51818ed42a5e0de0adcc14
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 22:32:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   // const token = c92d34b99b3130c0cae7c82af1a9ca02e52593a7
-  return '';
+  return token
 
 }
 
